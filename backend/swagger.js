ad8a79b7bcd0747e95df26dd939b981389a078bb
Mohamed Ghayyad <mohamedghayyad1@gmail.com>
2026-05-17 19:31:36 +0300

    Add Swagger API documentation

---
+++ backend/swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'BookNest API',
+      version: '1.0.0',
+      description: 'API documentation for BookNest Smart Library and Reading Progress System'
+    },
+    servers: [{ url: 'http://localhost:3000' }]
+  },
+  apis: ['./backend/routes/*.js']
+};
+
+const specs = swaggerJsdoc(options);
+
+function setupSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+}
+
+module.exports = setupSwagger;
